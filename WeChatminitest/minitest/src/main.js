346d0349f5e6a8da1e6ad080e82a1b80b296243e
Bingo <a1255230345@gmail.com>
2019-12-04 15:39:54 +0800

    提交项目

---
+++ WeChatminitest/minitest/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
+
+export default {
+  pages: [
+    '^pages/index/main'
+  ],
+  config: {
+    pages: [],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: '第一个小程序',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
