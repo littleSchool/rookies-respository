c1f8a1120346ce23be7cc42a38c9b63180e8ff1e
低調的D吖 <2289975022@qq.com>
2019-12-04 16:20:46 +0800

    up test

--- WeChatminitest/minitest/src/components/card.vue
+++ WeChatminitest/minitest/src/components/card.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
+  <div class="card"
     <p class="card">
       {{text}}
     </p>
+    <swiper :optops="swiperOption" class="swiper-menu">
+      <swiper-slide><img src="~assets/img/home.png" alt=""/></swiper-slide>
+      <swiper-slide><img src="~assets/img/home_1.png" alt=""/></swiper-slide>
+      <swiper-slide><img src="~assets/img/home_3.png" alt=""/></swiper-slide>
+      <div class="swiper-scrollbar" slot="scrollbar"></div>
+    </swiper>
   </div>
 </template>
 
