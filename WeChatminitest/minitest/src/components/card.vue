4644ad91f36a97450b16103393c34ed82278d382
低調的D吖 <2289975022@qq.com>
2019-12-06 09:51:30 +0800

    up Project Structure

--- WeChatminitest/minitest/src/components/card.vue
+++ WeChatminitest/minitest/src/components/card.vue
@@ -3,12 +3,7 @@
     <p class="card">
       {{text}}
     </p>
-    <swiper :optops="swiperOption" class="swiper-menu">
-      <swiper-slide><img src="~assets/img/home.png" alt=""/></swiper-slide>
-      <swiper-slide><img src="~assets/img/home_1.png" alt=""/></swiper-slide>
-      <swiper-slide><img src="~assets/img/home_3.png" alt=""/></swiper-slide>
-      <div class="swiper-scrollbar" slot="scrollbar"></div>
-    </swiper>
+    
   </div>
 </template>
 
